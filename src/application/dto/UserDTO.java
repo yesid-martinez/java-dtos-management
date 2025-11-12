78a6767ebc381c6910f58eec9d4660d54610a7c8
Yesid M <wujibifan.hh@gmail.com>
2025-11-12 13:26:35 -0500

    chore(dto): create User

---
+++ src/application/dto/UserDTO.java
@@ -0,0 +1,40 @@
+package application.dto;
+
+public class UserDTO {
+    private int id;
+    private String username;
+    private String email;
+    private long documentId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public long getDocumentId() {
+        return documentId;
+    }
+
+    public void setDocumentId(long documentId) {
+        this.documentId = documentId;
+    }
+}
