7333c812e4624b12abeab9a17360b49c66046986
Yesid M <wujibifan.hh@gmail.com>
2025-11-12 14:32:36 -0500

    chore(service): add UserService skeleton with placeholder methods

---
+++ src/application/service/UserService.java
@@ -0,0 +1,11 @@
+package application.service;
+
+public class UserService {
+    public void register(/*T dto*/) {
+            /* T user = T.createEntity(); */
+    }
+
+    public void /* List<T> */ getAllUsers(){
+        /* return T.getAll(); */
+    }
+}
